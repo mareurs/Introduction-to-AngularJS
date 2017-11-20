6b1aab167b42fc4bb4ebf8c179dc43e69bb6a89f
Marius Ailinca <ailinca.marius@gmail.com>
2017-11-20 14:50:01 +0200

    Fix showing 'Please enter the data first'

--- assignment1/assignment1-starter-code/src/app.js
+++ assignment1/assignment1-starter-code/src/app.js
@@ -10,17 +10,20 @@
   function as1Controller($scope){
     $scope.HealthyStatus = "";
     console.log("starting");
-    $scope.HealthyStatus = 'Please enter the data first';
-    $scope.healthyColor = "red";
 
     $scope.btnCheckClicked = function(){
-      var count = countTokens($scope.lunchMenu);
-      if(count > 3){
-        $scope.HealthyStatus = 'Too much!';
-      } else {
-        $scope.HealthyStatus = 'Enjoy!';
+      if($scope.lunchMenu){
+        var count = countTokens($scope.lunchMenu);
+        if(count > 3){
+          $scope.HealthyStatus = 'Too much!';
+        } else {
+          $scope.HealthyStatus = 'Enjoy!';
+        }
+        $scope.healthyColor = 'green';
       }
-      $scope.healthyColor = 'green';
+    } else {
+      $scope.HealthyStatus = 'Please enter the data first';
+      $scope.healthyColor = "red";
     }
   }
 
